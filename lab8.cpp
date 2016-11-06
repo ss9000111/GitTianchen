7eaa6de79edc41afd85e0477a24019a5b9432186
tianchen <tw5v8@mai.missouri.edu>
2016-11-05 19:11:35 -0500

    lab8 final version no part 3

--- lab8.cpp
+++ lab8.cpp
@@ -1,106 +1,152 @@
-
 #include <iostream>
 #include <stdio.h>
-#include <cstring>
+#include <string>
+// using library string
 using namespace std;
-
-class signal{
-private:
-	int i,length,max;
-	int total,n;
-	double average;
-	int *data;//private members
-	double average_data (){average = total/length; return average;};
-//auxiliary methods
-	void sig_info(){};
-	//display length, max and average;
+string datastring = "abcdefghijklmnopqrstuvwxyz";
+string moscodelib[26] = {".-","-...","-.-. "," -.. "," . "," ..-. "," --. "," .... "," .. "," .--- "," -.- "," .-.. "," -- "," -. "," --- "," .--. "," --.- "," .-. "," ... "," - "," ..- "," ...- "," .-- "," -..- "," -.-- "," --.."};
+// --------- message class and methods ------------------------------
+class message{
+protected: // accessible by derived classes, not by other users.
+string msg;
 public:
-	char name_in[20],name_out[20];
-	signal();
-	signal(int);
-	signal(char*);//constructor, default,input an int,input an file name
-	~signal();//destructor
-
-	void filename(char*);
-	void Offset(int);
-	void Scale(double);
-	void Center();
-	void Nomalize();
-	void Statistics();
-	void save_file(char*);//to create a text file with current data
+message(); // default constructor.
+message(string str); // parametric constructor
+~message(); // destructor
+void printinfo();
 };
-signal::signal(){
-	length = average = max= 0;
-	data = NULL;
-	cout << "\nDefault constructor" << endl;
-}
-signal::signal(int num){
-		sprintf(name_in,"Raw_data_%02d.txt",num);
-		cout << "\n constructor taking file number: "<<name_in << endl;
-}
-signal::signal(char* file){
 
-		cout << "\n constructor taking file name: " <<file<< endl;
+// Base class constructor
+message::message(){
+cout<< "default constructor"<<endl;
 }
-signal::~signal(){
-		delete[] data;
-		cout << "\n Destructor " << endl;
+// Base class constructor
+message::message(string str){
+msg = str;
+cout<< "parametic constructor"<<endl;
 }
 
+// Base class destructor
+message::~message(){
+cout << "destructor." << endl;
+}
 
-/*void signal::Offset(int i);
-void signal::Scale(double i);
-void signal::Center();
-void signal::Nomalize();
-void signal::Statistics();*/
-void signal::save_file(char* name){
-	cout<<"the file will be saved as name of:"<<name<<endl;
-	return;
-}//to create a text file with current data
+void message::printinfo() {
+cout<<"the string you entered is:" <<msg<<endl;
+}
+ //------------------------------------------------------------------
+
+ //--------- morsecodemessage class and methods ----------------------------
+class morsecodemessage : public message{ // morsecodemessage is derived from class message is a public base class
+protected:
+string *mosecode;
+public:
+morsecodemessage();
+morsecodemessage(string str);
+~morsecodemessage();
+// define new member function,called when initialization
+void translate(string str);
+// redefine member function
+void printinfo();
+};
 
 
 
+// Derived class constructor.
+morsecodemessage::morsecodemessage() : message() {
+	mosecode = NULL;
+cout<< "mosecodemessage default constructor"<<endl;
+}
+
+morsecodemessage::morsecodemessage(string str) : message(str) {
+mosecode = new string[str.length()];
+cout<< "you are entering  morsecode translator, the infor you entered and result will display"<<endl;
+
+}
+// Derived class destructor
+morsecodemessage::~morsecodemessage() {
+delete mosecode;
+cout << "destructor for morsecodemessage" << endl;
+}
+
+// Redefined printInfo function for derived class
+void morsecodemessage::printinfo(){
+cout<<"the string you entered is:" <<msg<<endl<<"the morsecode translated is:"<<mosecode<<endl;
+
+}
+void morsecodemessage::translate(string str)
+{
+int i,j;
+char *c;
+j = str.length();
+cout<<"string length is:"<<j<<endl;
+//there should be a pointer to the datastring which is the library for alphabets. but char *c always give me error
+cout<<"the morsecode translated is:"<<endl;
+		for (i=0; i<j;i++)
+		{
+			size_t index = datastring.find(c);
+		cout<< moscodelib[index];
+		}
+cout<<endl;
+}
+
+
+// --------- messageStack class and methods ------------------------------
+class messagestack{
+protected: // accessible by derived classes, not by other users.
+int size,top;
+int *stackPtr;
+public:
+messagestack(); // default constructor.
+messagestack(string str); // parametric constructor
+~messagestack(); // destructor
+void push();
+void pop();
+void printstack();
+
+};
+
+// Base class constructor
+messagestack::messagestack(){
+cout<< "default constructor"<<endl;
+}
+// Base class constructor
+messagestack::messagestack(string str){
+cout<< "parametic constructor"<<endl;
+}
+
+// Base class destructor
+messagestack::~messagestack(){
+cout << "destructor." << endl;
+}
+void messagestack::push(){
+
+}
+void messagestack::pop(){
+
+}
+void messagestack::printstack(){
+
+}
 
 
 
-int main(int argc, char *argv[]) {
-	using std::cout;
-	using std::endl;
-	cout << "Please enter the option 1.default constructor\n 2.constructor with input file number\n 3.constructor with input file name" << endl; // prints
-	int input;
-	char* filenameis;
-	cin>>input;
-	cout<< "the option you picked is number:"<<input<< endl;
-	switch(input)
-	{
-	case 1:
-	{
-		cout <<"deafult constructor called"<<endl;
-		signal signal1;
-		//call default constructor
-		break;//desructor called
-	}
-	case 2:
-	{
-		cout<<"please enter the file number you want to open"<<endl;
-		cin>>input;
-		signal signal2(input);		//call  constructor with input number
-		break;//desructor called
-	}
-	case 3:
-	{
-		cout<<"please enter the file name you want to open"<<endl;
-		cin>>filenameis;
-		signal signal3(filenameis);
-		break;//desructor called
-	}
-	default:
-	{
-		cout<<" invalid option please select from 1 to 3"<<endl;
-		break;
+// Main function. A few examples
+int main(){
+	cout<<"You are entering morsecode converter, please follow instruction to continue"<<endl;
+	cout<<"please enter a string (upper or lower case letters only )to conver to mosecode"<<endl;
+string strr;
+cin>>strr;
+message message1(strr);
+message1.printinfo();
 
-	}
-	}
-	cout<<"please input the file name you want to save as:"<<endl;
-	cin>>filenameis;
+
+cout<<"You are entering morsecode converter, please follow instruction to continue"<<endl;
+cout<<"please enter a string (upper or lower case letters only )to conver to mosecode"<<endl;
+cin>>strr;
+morsecodemessage mosemsg(strr);
+mosemsg.translate(strr);
+mosemsg.printinfo();
+
+return 0;
 }
